337fb742f6beca7522bc2862fcc3e05518eabf21
[firstname <j6cafe2018@gmail.com>
2024-03-01 00:19:13 +0800

    co

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -25,22 +25,6 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        // old
-        // $cart = Cart::where('user_id', Auth::user()->id)->whereIn('id', $request->input('id'))->get();
-
-        // $cart->each(function($data, $key) {
-        //     Order::create([
-        //         'user_id' => Auth::user()->id,
-        //         'quantity' => $data->quantity,
-        //         'product_variant_id' => $data->product_variant_id,
-        //     ]);
-        //     Cart::find($data->id)->delete();
-        // });
-
-        // return response()->json(['message' => 'success'], 200);
-
-
-
         $validator = Validator::make($request->all(), [
             'id.*' => 'exists:carts,id'
         ]);
@@ -48,38 +32,5 @@ class OrderController extends Controller
         if($validator->fails()) {
             return response()->json(['message' => $validator->messages()->get('*')], 500);
         }
-
-
-        $carts = Cart::whereIn('id', $request->input('id'))->get();
-        $variants = ProductVariant::whereIn('id', $carts->pluck('product_variant_id'))->get();
-
-        $carts->each(function($data, $key) {
-            Order::create([
-                'user_id' => Auth::user()->id,
-                'quantity' => $data->quantity,
-                'product_variant_id' => $data->product_variant_id,
-            ]);
-            Cart::find($data->id)->delete();
-        });
-
-        $data = [];
-        foreach ($variants as $variant) {
-            $data = array_merge($data, array($variant->stripe_price_id => 1));
-        }
-
-        // foreach ($carts as $cart) {
-        //     OrderItems::create([
-        //         'user_id' => Auth::id(),
-        //         'book_id' => $cart->book_id,
-        //         'order_id' => $order->id,
-        //     ]);
-        //     $cart->delete();
-        // }
-
-        // return $request->user()->checkout($data, [
-        //     'success_url' => route('payment.success'),
-        //     'cancel_url' => route('payment.cancel'),
-        // ]);
-
     }
 }

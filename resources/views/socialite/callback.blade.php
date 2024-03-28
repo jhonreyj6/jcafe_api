712102be6555ffe5f4c0b63e4370ecfeabd35b60
[firstname <j6cafe2018@gmail.com>
2024-03-28 20:17:16 +0800

    ded

--- resources/views/socialite/callback.blade.php
+++ resources/views/socialite/callback.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Callback</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/loginPusher.js'])
 </head>
 
 <body>
